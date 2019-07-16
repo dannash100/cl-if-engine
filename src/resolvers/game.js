5a78ed72e51b1eaf681c303888c1a3776f797fe8
dannash100 <danster15@gmail.com>
2019-07-16 13:06:34 +1200

    updateGame

--- src/resolvers/game.js
+++ src/resolvers/game.js
@@ -1,20 +1,29 @@
 export default {
   Query: {
-    getGames: () => Game.find(),
-    getGame: async (_, {id}) => {
-      const result = await Game.findById(id)
-      return result
+    Games: () => Game.find(),
+    Game: async (_, {id}) => {
+      const result = await Game.findById(id);
+      return result;
     }
   },
   Mutation: {
     addGame: async (_, {name, description, author}) => {
-      const game = new Game({name, description, author})
-      await game.save()
-      return game
+      const game = new Game({name, description, author});
+      await game.save();
+      return game;
+    },
+    updateGame: async (_, {id, name, description, author}) => {
+      // Try $set if not work
+      const result = await Game.findOneAndUpdate(
+        {_id: id},
+        {name, description, author},
+        {new: true}
+      );
+      return result
     },
     deleteGame: async (_, {id}) => {
-      await Game.findByIdAndRemove(id)
-      return "Game deleted"
+      await Game.findByIdAndRemove(id);
+      return "Game deleted";
     }
   }
-}
+};
